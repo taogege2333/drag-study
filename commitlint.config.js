cb5c6dfbf39bced57d5ccb750bb953dfa80ff233
Emotio <2523726756@qq.com>
2024-07-03 10:39:41 +0800

    build: 添加git可视化自定义提交规则

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,27 @@
 export default {
-	extends: ['@commitlint/config-conventional'],
-	rules: {
-		'type-enum': [
-			2,
-			'always',
-			[
-				'build', // 用于编译
-				'ci', // 用于提交 CI 任务
-				'chore', // 依赖更新/脚手架配置修改等
-				'docs', // 用于文档
-				'feat', // 用于新功能
-				'fix', // 用于 bug 修复
-				'perf', // 用于性能优化
-				'refactor', // 用于重构
-				'revert', // 用于回滚
-				'style', // 用于样式修改
-				'test', // 用于测试
-				'addLog', // 用于添加日志
-			],
-		],
-	},
+	// extends: ['@commitlint/config-conventional'],
+	// rules: {
+	// 	'type-enum': [
+	// 		2,
+	// 		'always',
+	// 		[
+	// 			'build', // 用于编译
+	// 			'ci', // 用于提交 CI 任务
+	// 			'chore', // 依赖更新/脚手架配置修改等
+	// 			'docs', // 用于文档
+	// 			'feat', // 用于新功能
+	// 			'fix', // 用于 bug 修复
+	// 			'perf', // 用于性能优化
+	// 			'refactor', // 用于重构
+	// 			'revert', // 用于回滚
+	// 			'style', // 用于样式修改
+	// 			'test', // 用于测试
+	// 			'addLog', // 用于添加日志
+	// 		],
+	// 	],
+	// },
+
+	// 自定义提交规范
+	extends: ['cz'],
+	rules: {},
 };

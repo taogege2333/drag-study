b31a8dff7912f4efae5029073047d5ca11ca18f6
Emotio <2523726756@qq.com>
2024-07-17 15:14:59 +0800

    fix: 修复按钮拖拽嵌套时原按钮dom未删除问题，ButtonWidget组件中注释导致

--- src/components/Widget/ButtonWidget/index.vue
+++ src/components/Widget/ButtonWidget/index.vue
@@ -1,6 +1,5 @@
 <template>
 	<el-button :type="widget.props.type" :size="widget.props.size">{{ widget.props.text }}</el-button>
-	<!-- <el-button :type="props.type" :size="props.size">{{ props.text }}</el-button> -->
 </template>
 
 <script setup lang="ts">
@@ -8,13 +7,9 @@ defineOptions({
 	name: 'ButtonWidget',
 });
 
-// import {useWidget} from '@/hooks/useWidget';
 import {ButtonWidgetType} from '@/types/button';
-// import {ButtonPropsType} from '@/types/button';
 import {computed} from 'vue';
 
-// const props = defineProps<ButtonPropsType>();
 const props = defineProps<{widget: ButtonWidgetType}>();
 const widget = computed(() => props.widget);
-// const {widget, currentWidget, handleClick} = useWidget<ButtonWidgetType>(props);
 </script>
